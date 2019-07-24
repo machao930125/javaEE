90a80e1463f38b128fe80b6f8a877926609bf4ae
machao <chao.ma@iquantex.com>
2019-07-24 20:50:12 +0800

    一致性hash

---
+++ src/main/java/com/chao/datastructure/consistent/Test.java
@@ -0,0 +1,7 @@
+package com.chao.datastructure.consistent;
+
+/**
+ * Created by Admin on 2019/7/18.
+ */
+public class Test {
+}
---
+++ src/main/java/com/chao/future/FutureDemo.java
@@ -0,0 +1,7 @@
+package com.chao.future;
+
+/**
+ * Created by Admin on 2019/7/18.
+ */
+public class FutureDemo {
+}
