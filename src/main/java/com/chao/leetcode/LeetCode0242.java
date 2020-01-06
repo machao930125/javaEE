567e399276b46abb010965d34b838f9cb4f1ac10
machao <machao1@9fbank.com.cn>
2020-01-06 21:03:08 +0800

    242

---
+++ src/main/java/com/chao/leetcode/LeetCode0242.java
@@ -0,0 +1,66 @@
+package com.chao.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+ * 示例 1:
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ */
+public class LeetCode0242 {
+    /**
+     * 通过数组排序进行比较
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean isAnagram1(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1, str2);
+    }
+
+    /**
+     * 通过数组计数统计
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public static boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static class Test {
+        public static void main(String[] args) {
+            String s = "anagram", t = "nagaram";
+            boolean anagram1 = isAnagram1(s, t);
+            boolean anagram2 = isAnagram2(s, t);
+            System.out.println("执行结果：" + anagram1 + "," + anagram2);
+        }
+    }
+
+}
